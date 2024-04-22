80c25393c9cf08e49a32f527905fe554832fa69d
Feryal Doubali <feryalhalima@gmail.com>
2024-04-22 17:28:24 +0200

    yes

--- models/Task.model.js
+++ models/Task.model.js
@@ -2,11 +2,22 @@ const { Schema, model } = require("mongoose");
 
 const taskSchema = new Schema(
   {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
     title: {
       type: String,
       //required: [true, "Task title is required."],
       trim: true,
     },
+    projects: [
+      {
+        type: Schema.Types.ObjectId,
+        //required: true,
+        ref: "Project._id",
+      },
+    ],
     description: {
       type: String,
       trim: true,
--- seed/tasks.seed.js
+++ seed/tasks.seed.js
@@ -5,24 +5,28 @@ const Task = require("../models/Task.model.js");
 
 const tasks = [
   {
+    user: new user(),
     title: "make the Exceltable of the project budget",
     description: " make the profit calculation of the year",
     status: "todo",
     startDate: new Date(),
   },
   {
+    user: new user(),
     title: "Prepare presentation for stakeholders meeting",
     description: "Create slides and gather necessary data",
     status: "in progress",
     startDate: new Date(),
   },
   {
+    user: new user(),
     title: "Review and finalize project proposal",
     description: "Review proposal document and make necessary revisions",
     status: "done",
     startDate: new Date(),
   },
   {
+    user: new user(),
     title: "Schedule project kick-off meeting",
     description:
       "Coordinate with team members and stakeholders to set meeting date and agenda",
@@ -30,24 +34,28 @@ const tasks = [
     startDate: new Date(),
   },
   {
+    user: new user(),
     title: "Conduct market research",
     description: "Gather data on market trends and competitors",
     status: "done",
     startDate: new Date(),
   },
   {
+    user: new user(),
     title: "Implement user authentication system",
     description: "Develop login and registration functionality",
     status: "in progress",
     startDate: new Date(),
   },
   {
+    user: new user(),
     title: "Write API documentation",
     description: "Document the endpoints and usage of the API",
     status: "todo",
     startDate: new Date(),
   },
   {
+    user: new user(),
     title: "Test application on multiple devices",
     description:
       "Perform compatibility testing on various devices and browsers",
@@ -55,12 +63,14 @@ const tasks = [
     startDate: new Date(),
   },
   {
+    user: new user(),
     title: "Optimize database queries",
     description: "Improve the performance of database queries",
     status: "in progress",
     startDate: new Date(),
   },
   {
+    user: new user(),
     title: "Deploy application to production server",
     description: "Deploy the application to the live server environment",
     status: "todo",
